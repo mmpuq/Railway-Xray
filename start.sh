128400d0fea6ce0eff94817b63abe7f893619779
ChirmyRam <34760505+ChirmyRam@users.noreply.github.com>
2021-11-06 19:51:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ StoreFiles=https://raw.githubusercontent.com/jth445600/HerokuXray/main/etc/Store
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >xray.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
@@ -24,6 +24,6 @@ done
 # start
 tor &
 
-/xray -config /xray.json &
+./xray -config xray.json &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
