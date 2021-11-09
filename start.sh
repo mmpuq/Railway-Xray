e1a4d7bf7542a71a42bc2acbd9f96b8e1721e8ab
ChirmyRam <34760505+ChirmyRam@users.noreply.github.com>
2021-11-09 11:53:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
 wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+chmod -R 777 /usr/share/caddy
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
@@ -26,4 +27,4 @@ tor &
 
 /xray -config /xray.json &
 
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+sudo caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
